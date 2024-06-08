eec3f8a5917feed0fe9c4b87d7961c3cb4f1a52c
Decargar <edecargarb@gmail.com>
2024-06-08 10:21:59 -0700

    added number of attempt left

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 let attempts = 5;
+let currentAttempt = 1;
 
 function makeGuess() {
     let userGuess = document.getElementById('userGuess').value;
     let message = '';
-    
-    if (attempts > 1) {
+
+    if (currentAttempt <= 5) {
         if (userGuess == randomNumber) {
             message = 'Congratulations! You guessed the right number!';
             attempts = 0; // End the game
@@ -16,16 +17,17 @@ function makeGuess() {
             message = 'Too low! Try again.';
             attempts--;
         }
-    } else {
-        if (userGuess == randomNumber) {
-            message = 'Congratulations! You guessed the right number!';
-        } else {
-            message = `Sorry, you've used all your attempts. The correct number was ${randomNumber}.`;
-        }
+
+        currentAttempt++;
+    }
+
+    if (currentAttempt > 5 && userGuess != randomNumber) {
+        message = `Sorry, you've used all your attempts. The correct number was ${randomNumber}.`;
         attempts = 0; // End the game
     }
     
     document.getElementById('message').innerText = message;
+    document.getElementById('guessCount').innerText = currentAttempt > 5 ? 5 : currentAttempt;
     document.getElementById('attemptsLeft').innerText = `Attempts left: ${attempts}`;
     
     if (attempts == 0) {
